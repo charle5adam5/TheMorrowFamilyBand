dcef67c84964152d56b0a2dfe0bc55432fb3bd0d
Charles <adams.charles00@gmail.com>
2017-11-11 18:42:35 -0500

    edits

--- album.js
+++ album.js
@@ -1,40 +1,48 @@
 
 var photo = document.getElementById("album");
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
 
 var album = [
   "./images/gallery1.JPG",
+  "./images/flavors.jpg",
   "./images/gallery2.JPG",
   "./images/gallery3.JPG",
+  "./images/flavors2.jpg",
   "./images/gallery4.JPG",
   "./images/gallery5.JPG",
+  "./images/fullband.jpg",
   "./images/gallery6.JPG",
   "./images/gallery7.JPG",
+  "./images/fullband2.jpg",
   "./images/gallery8.JPG",
   "./images/gallery9.JPG",
+  "./images/flavors3.jpg",
   "./images/gallery10.JPG",
-  "./images/gallery11.JPG"
+  "./images/gallery11.JPG",
+  "./images/flavors4.jpg"
 ];
+var count = 0;
+photo.src = album[count];
 
-function albumFlip() {
-  var curIndex = 0;
-  photo.src = album[curIndex];
-  setInterval(function() {
-    curIndex++;
-    if(curIndex >= album.length) {
-      curIndex = 0;
-    }
-    if(photo.className != "fade") {
-        photo.className = "fade";
-      setTimeout(function() {
-        photo.className = "";
-      }, 1000);
-    }
-    photo.src = album[curIndex];
-  }, 5000);
-};
+console.log(album.length);
 
-album.forEach(function(img) {
-  img = new Image();
-  img.onload = albumFlip();
-})
-// photo.addEventListener("load", albumFlip());
+
+(function images() {
+  next.addEventListener("click", function() {
+    count++;
+    console.log(count);
+    if(count > album.length-1) {
+      count = 0;
+    }
+    photo.src = album[count];
+  });
+  prev.addEventListener("click", function() {
+    count--;
+    console.log(count);
+    if(count < 0) {
+      count = album.length-1;
+    }
+    photo.src = album[count];
+  });
+})();
