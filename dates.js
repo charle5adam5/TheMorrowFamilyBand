2ea97d66da04331bd26db64fe1608547c92af3ac
Charles <adams.charles00@gmail.com>
2018-03-10 15:31:39 -0500

    add schedule js and style

---
+++ dates.js
@@ -0,0 +1,48 @@
+let dates = [
+  {
+    year: 2018,
+    month: 12,
+    day: 22,
+  },
+  {
+    year: 2018,
+    month: 12,
+    day: 21,
+  },
+  {
+    year: 2018,
+    month: 11,
+    day: 17,
+  },
+  {
+    year: 2018,
+    month: 11,
+    day: 16,
+  },
+  {
+    year: 2018,
+    month: 9,
+    day: 15,
+  },
+  {
+    year: 2018,
+    month: 4,
+    day: 6,
+  },
+];
+
+let date = document.querySelectorAll('.date');
+let gDates = document.querySelectorAll('.gig');
+
+let today = new Date();
+  date.forEach(function(item, index) {
+    let gigDate = new Date(
+        dates[index].year,
+        dates[index].month - 1,
+        dates[index].day
+      );
+      item.innerHTML = gigDate.toLocaleDateString();
+      if(gigDate > today) {
+        gDates[index].classList.add('upcoming');
+      }
+    })
